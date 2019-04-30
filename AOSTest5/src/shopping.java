00154dcd45bab590a79caf8103f85e43c2fed8c3
Admin Default <admin@default.com>
2019-04-30 16:37:13 -0600

    Speakers test ready

--- AOSTest5/src/shopping.java
+++ AOSTest5/src/shopping.java
@@ -25,37 +25,19 @@ public class shopping {
 
 	@Test
 	public void speakers() {
-/*
-		desktop.<DomElement>find("nimbusserver.BrowserWindow.speakersTxt").click(MouseButton.LEFT, new Point(100, 15));
 
-		desktop.<DomElement>find("nimbusserver.BrowserWindow.20").click();
-		//desktop.<DomLink>find("nimbusserver.BrowserWindow.Bose Soundlink Bluet").click(MouseButton.LEFT,
-		//		new Point(83, 11));
-
-		DomElement _26999 = desktop.<DomElement>find("nimbusserver.BrowserWindow.$269 99");
-		Assert.assertEquals("$269.99", _26999.getProperty("textContents"));
-
-		desktop.<DomButton>find("nimbusserver.BrowserWindow.save_to_cart").click(MouseButton.LEFT, new Point(149, 20));
-		desktop.<DomElement>find("nimbusserver.BrowserWindow.menuCart").click(MouseButton.LEFT, new Point(13, 9));
-		DomElement _26999Span = desktop.<DomElement>find("nimbusserver.BrowserWindow.$269 99 Span");
-		Assert.assertEquals("$269.99", _26999Span.getProperty("textContents"));
-
-		desktop.<DomLink>find("nimbusserver.BrowserWindow.REMOVE").click(MouseButton.LEFT, new Point(21, 16));
-		desktop.<DomLink>find("nimbusserver.BrowserWindow.CONTINUE SHOPPING").click(MouseButton.LEFT,
-				new Point(120, 17));
-*/
 		//start recording 
-		desktop.<DomElement>find("nimbusserver.BrowserWindow.speakersImg").click(MouseButton.LEFT, new Point(249, 160));
-		desktop.<DomElement>find("nimbusserver.BrowserWindow.20").click(MouseButton.LEFT, new Point(98, 74));
+		desktop.<DomElement>find("nimbusserver.BrowserWindow.speakersImg").click(MouseButton.LEFT, new Point(300, 170));
+		desktop.<DomElement>find("nimbusserver.BrowserWindow.20").click(MouseButton.LEFT, new Point(107, 62));
 		DomElement _26999 = desktop.<DomElement>find("nimbusserver.BrowserWindow.$269 99");
 		Assert.assertEquals("$269.99", _26999.getProperty("textContents"));
-		desktop.<DomButton>find("nimbusserver.BrowserWindow.save_to_cart").click(MouseButton.LEFT, new Point(172, 27));
-		desktop.<DomElement>find("nimbusserver.BrowserWindow.menuCart").click(MouseButton.LEFT, new Point(16, 10));
+		desktop.<DomButton>find("nimbusserver.BrowserWindow.save_to_cart").click(MouseButton.LEFT, new Point(161, 19));
+		desktop.<DomElement>find("nimbusserver.BrowserWindow.menuCart").click(MouseButton.LEFT, new Point(15, 3));
 		DomElement _26999Span = desktop.<DomElement>find("nimbusserver.BrowserWindow.$269 99 Span");
 		Assert.assertEquals("$269.99", _26999Span.getProperty("textContents"));
-		desktop.<DomLink>find("nimbusserver.BrowserWindow.REMOVE").click(MouseButton.LEFT, new Point(13, 13));
+		desktop.<DomLink>find("nimbusserver.BrowserWindow.REMOVE").click(MouseButton.LEFT, new Point(23, 15));
 		desktop.<DomLink>find("nimbusserver.BrowserWindow.CONTINUE SHOPPING").click(MouseButton.LEFT,
-				new Point(120, 18));
+				new Point(98, 16));
 		//end recording
 	}
 
